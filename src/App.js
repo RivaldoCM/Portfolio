7c92da431b7a03b180f9177951e8e7e616840fa6
RivaldoCardoso <rivaldocardoso2010@hotmail.com>
2022-10-30 01:27:01 -0300

    iniciando projeto, react configurado.

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { Menu } from "./components/menu";
+import { GlobalStyle } from "./globalStyle/global";
+
+export function App() {
+  return (
+    <div className="App">
+      <Menu />
+      <GlobalStyle />
+    </div>
+  );
+}
---
+++ src/components/menu/index.jsx
@@ -0,0 +1,10 @@
+import { Nav } from "./style"
+
+export function Menu(){
+    return(
+        <Nav>
+            
+        </Nav>
+    )
+
+}
